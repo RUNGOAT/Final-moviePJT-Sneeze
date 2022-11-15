1db87ebe334076179fd130fb8fd2638c4a2bb233
Chaeeun Lee <chaen511@naver.com>
2022-11-15 17:44:54 +0900

    getMovies

--- vue-frontend/src/views/home/HomeView.vue
+++ vue-frontend/src/views/home/HomeView.vue
@@ -13,8 +13,22 @@ export default {
   name: 'HomeView',
   components: {
     CarouselList,
+  },
+  created() {
+    this.getMovies()
+  },
+  methods: {
+    getMovies() {
+      // if (this.isLogin) {
+        this.$store.dispatch('getMovies')
+      // } else {
+      //   alert('로그인이 필요한 서비스 입니다.')
+      //   this.$router.push({ name: 'LogInView'})
+      }
+    }
   }
-}
+// }
+
 </script>
 
 <style>
