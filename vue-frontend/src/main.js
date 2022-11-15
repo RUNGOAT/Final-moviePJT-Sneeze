976f0068154ddb25374d914a2269efa55aeb24ae
Jaegun Seo <sjk1052005@gmail.com>
2022-11-15 11:14:49 +0900

    start

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 Vue.config.productionTip = false
 
