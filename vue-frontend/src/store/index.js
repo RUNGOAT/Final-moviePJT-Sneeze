533e7eac56ce46d9fbb33fc709292871cb118cfd
Jaegun Seo <sjk1052005@gmail.com>
2022-11-16 10:51:08 +0900

    detail store index cosMoive add

--- vue-frontend/src/store/index.js
+++ vue-frontend/src/store/index.js
@@ -5,11 +5,14 @@ import axios from 'axios'
 Vue.use(Vuex)
 
 const API_URL = 'http://127.0.0.1:8000'
+const API_KEY = 'c12ca67b05f1378e09cf647da6b26b3e'
+
 
 export default new Vuex.Store({
   state: {
     movies: [],
     movieGenre: [],
+    cosMovies: [],
 
   },
   getters: {
@@ -17,6 +20,9 @@ export default new Vuex.Store({
   mutations: {
     GET_MOVIES(state, movies) {
       state.movies = movies
+    },
+    GET_COS_MOVIE(state, cosMovies) {
+      state.cosMovies = cosMovies
     },
   },
   actions: {
@@ -33,6 +39,16 @@ export default new Vuex.Store({
           context.commit('GET_MOVIES', res.data)
         })
         .catch(err => console.log(err))
+    },
+    getCosMovie(context, movie_id){
+      axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${API_KEY}&language=ko-KR&page=1`
+      })
+        .then(res => {
+          // console.log(res.data.results)
+          context.commit('GET_COS_MOVIE', res.data.results)
+        })
     },
   },
   modules: {
--- vue-frontend/src/views/movie/MovieDetailView.vue
+++ vue-frontend/src/views/movie/MovieDetailView.vue
@@ -10,9 +10,30 @@ import ReviewList from '@/views/movie/ReviewList'
 
 export default {
   name: 'MovieDetailView',
+  props: {
+    movie: Object,
+  },
   components: {
     ReviewList,
-  }
+  },
+  data() {
+    return {
+      imageUrl: 'https://image.tmdb.org/t/p/w1280'
+    }
+  },
+  computed: {
+    recoMovies() {
+      return this.$store.state.cosMovies
+    }
+  },
+  methods: {
+    getCosMovie() {
+	      this.$store.dispatch('getCosMovie', this.movie.id)  // 디테일 페이지의 영화 id
+    }
+  },
+  created() {
+    this.getCosMovie()
+  },
 }
 </script>
 
