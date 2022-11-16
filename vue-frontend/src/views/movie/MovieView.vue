4e1184c40c6f3123b7b1b449067c1cbfa44ca662
Chaeeun Lee <chaen511@naver.com>
2022-11-16 10:51:07 +0900

    movie detail 수정

---
+++ vue-frontend/src/views/movie/MovieView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Movie</h1>
+    <MovieCardView/>
+  </div>
+</template>
+
+<script>
+import MovieCardView from '@/views/movie/MovieCardView'
+export default {
+  name: 'MovieView',
+  components: {
+    MovieCardView
+  },
+}
+</script>
+
+<style>
+
+</style>
