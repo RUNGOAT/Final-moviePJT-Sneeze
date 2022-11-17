fdaec5add0f4cf8f7f3ea736baf9ba8d721b849e
Jaegun Seo <sjk1052005@gmail.com>
2022-11-17 09:26:15 +0900

    commentform ing

---
+++ vue-frontend/src/components/CommunityCommentForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit.prevent="createComment">
+      <label for="content">댓글: </label>
+      <input type="text" id="content" v-model.trim="content">
+      <button>작성</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'CommunityCommentForm',
+  data() {
+    return {
+      content: null,
+    }
+  },
+  methods: {
+    createComment() {
+      axios({
+        method: 'get',
+        // url: 
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
