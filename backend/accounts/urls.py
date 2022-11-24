68839175431eab9c906d3ca38adae2c9f8871488
Jaegun Seo <sjk1052005@gmail.com>
2022-11-24 09:46:25 +0900

    view component clear

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,15 +4,6 @@ from . import views
 
 app_name = 'accounts'
 
-# urlpatterns = [
-#     path('users/', views.users),
-#     # path('signup/', views.signup),
-#     path('api-token-auth/', obtain_jwt_token),
-#     path('myprofile/', views.my_profile),
-#     path('follow/<int:my_pk>/<int:user_pk>/', views.follow),
-#     path('info/', views.users_info),
-#     path('<username>/', views.profile),
-# ]
 urlpatterns = [
     path('users/', views.users),
     path('user/<int:my_pk>/', views.user),
